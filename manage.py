a6e0e10ac8b7d234a60b50eeb32c0756242fc760
Hardik-Katyal <hardikkatyalofficial@gmail.com>
2025-09-14 05:18:14 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,10 +8,13 @@ csv_files = glob.glob('data/*.csv')
 df = pd.concat([pd.read_csv(file) for file in csv_files], ignore_index=True)
 print()
 
+#df is df where the product in df is labeled as pink morsel
 df = df[df['product'] == 'Pink Morsel']
 
+#new slaes column
 df['sales'] = df['price'] * df['quantity']
 
+#removing unwanted columns
 df = df[['sales', 'date', 'region']]
 
 df.to_csv('.data/daily_sales_data_pinkMorsel.csv', index=False)
